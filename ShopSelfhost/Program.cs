83a473ef4f3478e556a3976d71bf2e8764c3a03b
Julia Henrich <Julia-Henrich@live.nmit.ac.nz>
2019-05-29 12:37:31 +1200

    install selfhost nuget

--- ShopSelfhost/Program.cs
+++ ShopSelfhost/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
 
 namespace ShopSelfhost
 {
@@ -10,6 +12,22 @@ namespace ShopSelfhost
     {
         static void Main(string[] args)
         {
+            // Set up server configuration
+            Uri _baseAddress = new Uri("http://localhost:60064/");
+            HttpSelfHostConfiguration config = new HttpSelfHostConfiguration(_baseAddress);
+            config.Routes.MapHttpRoute(
+            name: "DefaultApi",
+            routeTemplate: "api/{controller}/{action}/{id}",
+            defaults: new { id = RouteParameter.Optional }
+            );
+            // Create server
+            HttpSelfHostServer server = new HttpSelfHostServer(config);
+            // Start listening
+            server.OpenAsync().Wait();
+            Console.WriteLine("Gallery Web-API Self hosted on " + _baseAddress);
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+            server.CloseAsync().Wait();
         }
     }
 }
